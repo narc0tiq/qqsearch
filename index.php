245ce5b860f2bb29ac8a27f1816484b9f17406c7
unknown <narc@.internal.narc.ro>
2009-03-27 19:32:33 +0200

    Removed skeleton testing logic.

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?PHP
 // Primary driver for the QQSearch. Currently, just pseudo-logic.
 
+define('PATH_BASE', dirname(__FILE__));
+
 // - includes
 include('inc/base_class.inc');
-
-// Testing the skeleton:
-$qq_builtins = array();
-include('builtins/skel.inc');
-
-foreach($qq_builtins as $b)
-{
-	$b->execute(null);
-}
+include('inc/base.inc');
 
 // - initialize:
 // -- check for databases
 // -- disable aliases if they're missing
 // -- complain if there's no database at all
 // -- include the builtins
+$qq_builtins = qq_get_builtins();
+
 // - check for a command; if none, default to "none"
 // - parse the command
 // - check what should be called for it and call it
