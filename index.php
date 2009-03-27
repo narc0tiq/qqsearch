566c9674f8d2b10ca630fe95250d6f255a113a77
unknown <narc@.internal.narc.ro>
2009-03-27 16:52:48 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?PHP
+// Primary driver for the QQSearch. Currently, just pseudo-logic.
+
+// - includes
+// - initialize:
+// -- check for databases
+// -- disable aliases if they're missing
+// -- complain if there's no database at all
+// -- include the builtins
+// - check for a command; if none, default to "none"
+// - parse the command
+// - check what should be called for it and call it
+// - finalize?
+
+?>
