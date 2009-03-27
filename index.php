70a18f7341cbba37fdefd78fbf049673b4b6691a
unknown <narc@.internal.narc.ro>
2009-03-27 21:58:52 +0200

    Moved most of the base logic to a class instead of leaving it hanging out in index.php. Possibly a bit excessive on the OO there, I dunno.

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?PHP
-// Primary driver for the QQSearch. Currently, just pseudo-logic.
+// QQSearch endpoint. Uses QQEngine to do the real work.
 
 define('PATH_BASE', dirname(__FILE__));
 
 // - includes
 include('inc/base_class.inc');
 include('inc/base.inc');
+include('inc/sql.inc');
+include('inc/engine.inc');
 
-// - initialize:
-// -- check for databases
-// -- disable aliases if they're missing
-// -- complain if there's no database at all
-// -- include the builtins
-$qq_builtins = qq_get_builtins();
+$engine = new QQEngine;
 
-// - check for a command; if none, default to "none"
-// - parse the command
-// - check what should be called for it and call it
-// - finalize?
+$engine->run();
 
 ?>
