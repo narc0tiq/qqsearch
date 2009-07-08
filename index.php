64aaa335d66d61797cb2500dcd185005d16bc90f
Octav Sandulescu <narc@hermes.internal.narc.ro>
2009-07-08 18:35:00 +0300

    Made includes rely on absolute paths (instead of relative), set error_reporting to E_ALL and fixed the edge case where q=(empty)

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?PHP
 // QQSearch endpoint. Uses QQEngine to do the real work.
 
+error_reporting(E_ALL);
+
 define('PATH_BASE', dirname(__FILE__));
 
-include('inc/base_class.inc');
-include('inc/base.inc');
-include('inc/sql.inc');
-include('inc/engine.inc');
+include(PATH_BASE.'/inc/base_class.inc');
+include(PATH_BASE.'/inc/base.inc');
+include(PATH_BASE.'/inc/sql.inc');
+include(PATH_BASE.'/inc/engine.inc');
 
 $engine = new QQEngine;
 
-$engine->run($_GET['q']);
+$query='';
+if(!empty($_GET['q']))
+	$query = $_GET['q'];
+$engine->run($query);
 
 ?>
